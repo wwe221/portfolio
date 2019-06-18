c99785c412ea81ef8fca69c3eb23c98be20882cd
wwe221 <wwe22123@gmail.com>
2019-06-18 17:36:34 +0900

    포트폴리오 1.01 수정

--- js/myjs.js
+++ js/myjs.js
@@ -1,12 +1,12 @@
 /**
  * 2019.06.18
  */
-function myShowFunction() {
-	var test1 = document.getElementById("panel").style.display;	
+function myShowFunction(id) {
+	var test1 = document.getElementById(id).style.display;	
 	if (test1 == 'none'|| test1=='') {
-		document.getElementById("panel").style.display = "block";
+		document.getElementById(id).style.display = "block";
 	} else {
-		document.getElementById("panel").style.display = "none";
+		document.getElementById(id).style.display = "none";
 	}
 
 }
